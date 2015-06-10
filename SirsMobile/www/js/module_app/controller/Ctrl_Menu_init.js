2d061c891eb1f56226e7abf93b10509303943c3d
rdardie <roch.dardie@geomatys.com>
2015-06-10 15:56:47 +0200

    feat (database) : add replicator initial + loading on ui

--- SirsMobile/www/js/module_app/controller/Ctrl_Menu_init.js
+++ SirsMobile/www/js/module_app/controller/Ctrl_Menu_init.js
@@ -2,20 +2,33 @@
  * Created by roch dardie on 28/05/15.
  */
 
+//TODO nommer controller
+//TODO créer la doc pas default
 
 angular.module('ctrl.menu.init', [])
 
-    .controller('InitCtrl', function ($scope, $log, sPouch,$state) {
+    .controller('InitCtrl', function InitCtrl($scope, $log, sPouch,$state) {
 
         var me = this;
+
+        //me.loading = true;
+        me.syncState = sPouch.syncState;
 
         //me.oUrlCdb = new oUrlCouchDb();
 
         //TODO debug only
         me.oUrlCdb = new oUrlCouchDb("geouser","geopw","5.196.17.92",5984,"sirs_isere");
-        //
-        me.syncro= function(oUrlCdb){
-                sPouch.dbConf(oUrlCdb);
+
+
+        /**
+         * @param {oUrlCouchDb} oUrlCdb
+         */
+        me.syncro= function(){
+                sPouch.dbInit(me.oUrlCdb );
+        };
+
+        me.clearDb= function(oUrlCdb){
+            sPouch.destroyDb(oUrlCdb);
         };
 
         me.note= function(note){
--- SirsMobile/www/module_rde/data/services/dataServicesSource.js
+++ SirsMobile/www/module_rde/data/services/dataServicesSource.js
@@ -3,7 +3,7 @@
  */
 angular.module('module_rde.data.services.source', [])
 
-    .service('sPouch', function (pouchDB, $log, $rootScope) {
+    .service('sPouch', function sPouch (pouchDB, $log, $rootScope, $timeout, $http) {
 
         var me = this;
 
@@ -14,52 +14,177 @@ angular.module('module_rde.data.services.source', [])
         me.localDb =  null;
 
         //objet pour couper la syncro
-        me.synclocalDb = null;
+        me.syncInstanceColector = {};
+
+        me.syncState = {actual:0, total:0, ratio:0 ,  clonning:false}
 
 
 
         //instancie une syncro bi-directionelle avec support de l'interuption, et propagation des evenement change
             //les evenements sont nome comme la base
         me.initiateSync = function (oUrlCdb) {
-            return PouchDB.sync("" + oUrlCdb.db, oUrlCdb.getUrlString(), {
+            $log.info("RUN_db_SYNC");
+            me.syncInstanceColector[oUrlCdb.db] = PouchDB.sync("" + oUrlCdb.db, oUrlCdb.getUrlString(), {
                 live: true,
                 retry: true
             }).on('change', function (info) {
                 // handle change
-                $log.info(oUrlCdb.db + '_change');
+                $log.info(oUrlCdb.db +'_Sync_'+ '_change');
                 $log.debug(info);
                 if (info.direction == "pull") {
                     $rootScope.$broadcast(oUrlCdb.db + "_change"); //FIXME  ne pas declacher l'event lorsque la modification vien du local.
                 }
             }).on('paused', function () {
                 // replication paused (e.g. user went offline)
-                $log.info(oUrlCdb.db + '_paused');
+                $log.info(oUrlCdb.db +'_Sync_'+ '_paused');
             }).on('active', function () {
                 // replicate resumed (e.g. user went back online)
-                $log.info(oUrlCdb.db + '_active');
+                $log.info(oUrlCdb.db + '_Sync_'+'_active');
             }).on('denied', function (info) {
                 // a document failed to replicate, e.g. due to permissions
-                $log.error(oUrlCdb.db + '_denied');
+                $log.error(oUrlCdb.db +'_Sync_'+ '_denied');
                 $log.error(info);
             }).on('complete', function (info) {
                 // handle complete
-                $log.info(oUrlCdb.db + '_complete');
+                $log.info(oUrlCdb.db +'_Sync_'+ '_complete');
                 $log.info(info);
             }).on('error', function (err) {
                 // handle error
-                $log.error(oUrlCdb.db + '_error');
+                $log.error(oUrlCdb.db +'_Sync_'+ '_error');
                 $log.error(error);
             });
         };
 
 
+        me.instantiateRep = function(oUrlCdb,syncMe){
+            $log.info("RUN_db_REP");
+            me.syncState.clonning=true;
+
+            //var RemoteDb = new PouchDB(oUrlCdb.getUrlString());
+
+
+            me.localDb.replicate.from( oUrlCdb.getUrlString(), {
+                live: false,
+                retry: true
+            }).on('change', function (info) {
+                $log.info(oUrlCdb.db +'_Repliation_'+ '_paused');
+            }).on('paused', function () {
+                // replication paused (e.g. user went offline)
+                $log.info(oUrlCdb.db +'_Repliation_'+ '_paused');
+            }).on('active', function () {
+                // replicate resumed (e.g. user went back online)
+                $log.info(oUrlCdb.db +'_Repliation_'+ '_active');
+            }).on('denied', function (info) {
+                // a document failed to replicate, e.g. due to permissions
+                $log.error(oUrlCdb.db +'_Repliation_'+ '_denied');
+                $log.error(info);
+            }).on('complete', function (info) {
+                // handle complete
+                $log.info(oUrlCdb.db + '_Repliation_'+'_complete');
+                $log.info(info);
+
+                me.syncState.clonning = false
+
+                if(syncMe === true) me.initiateSync(oUrlCdb);
+
+
+
+            }).on('error', function (err) {
+                // handle error
+                $log.error(oUrlCdb.db + '_Repliation_'+'_error');
+                $log.error(error);
+
+                me.syncState.clonning = false
+            });
+
+
+            me._recursiveRegularGetState();
+
+
+
+        }
+
+
+
+       me._recursiveRegularGetState =  function(){
+           $log.debug("RUN__recursiveRegularGetState")
+           $log.debug("RUN__recursiveRegularGetState")
+
+
+            me.localDb.info().then( function(result){
+                $log.debug("RUN__info")
+                $log.debug( result)
+                $log.debug( me.syncState.clonning)
+                $log.debug(me.syncState.actual)
+                $log.debug(result.doc_count);
+
+                me.syncState.actual= (result.doc_count / me.syncState.ratio).toFixed(2);
+
+                if(me.syncState.clonning == true){
+                    $timeout( me._recursiveRegularGetState,2000);
+
+                }
+
+
+            });
+
+
+       }
 
         me.dbConf = function(oUrlCdb){
             me.localDb = new pouchDB(oUrlCdb.db);
-            me.synclocalDb =   me.initiateSync(oUrlCdb);
+                me.initiateSync(oUrlCdb);
+            $log.debug("instance de base");
+            $log.debug(me.syncInstanceColector);
+
+
+        };
+
+        me.dbInit = function(oUrlCdb){
+
+            $http.get(oUrlCdb.getUrlString()).
+                success(function(data, status, headers, config) {
+                    $log.debug("gettotal")
+                    $log.debug(data);
+                    me.syncState.total = data.doc_count;
+                    me.syncState.ratio = data.doc_count / 100;
+                }).
+                error(function(data, status, headers, config) {});
+
+
+            me.localDb = new pouchDB(oUrlCdb.db);
+            me.instantiateRep( oUrlCdb,true  );
+
+        };
+
+        me.destroyDb = function(oUrlCdb){
+            $log.debug("RUN__clear")
+
+            me.localDb = new pouchDB(oUrlCdb.db);
+            me.localDb.destroy().then(function () {
+                $log.debug("db_clear")
+            }).catch(function (error) {
+                console.log(error);
+            });
 
         };
 
 
 
+
+
+            //$http.get(oUrlCdb.getUrlString()).
+            //    success(function(data, status, headers, config) {
+            //        $log.debug(data)
+            //        //me.syncState.total =
+            //    }).
+            //    error(function(data, status, headers, config) {
+            //        // called asynchronously if an error occurs
+            //        // or server returns response with an error status.
+            //    });
+
+
+
     })
+
+
