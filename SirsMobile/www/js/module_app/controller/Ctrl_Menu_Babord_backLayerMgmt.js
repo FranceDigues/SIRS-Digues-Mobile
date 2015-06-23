a22872f2659dfd41dee53697d9ea662dedc2844c
rdardie <roch.dardie@geomatys.com>
2015-06-23 18:56:41 +0200

    feat (backLayerMgmt) :  add control on layer & aquire layer in db

--- SirsMobile/www/js/module_app/controller/Ctrl_Menu_Babord_backLayerMgmt.js
+++ SirsMobile/www/js/module_app/controller/Ctrl_Menu_Babord_backLayerMgmt.js
@@ -6,16 +6,28 @@
 
 angular.module('module_app.controllers.menus.babord.backLayerMgmt', [])
 
-    .controller('cBackLayerMgmt', function cBackLayerMgmt($scope, $state, $log, sContext) {
+    .controller('cBackLayerMgmt', function cBackLayerMgmt($scope, $state, $log, sContext, sMapLayer) {
         var me = this;
 
         me.sContext = sContext;
+        me.sMapLayer = sMapLayer;
 
         me.goToGeoCache = function(){
             $log.debug("goto geoCache")
             $state.go('geoCache')
         }
 
+
+        me.updateBaseLayer=function(layerName){
+
+            for(var i=0 ; i< me.sMapLayer.list.length ; i++)
+
+                if( me.sMapLayer.list[i].name === layerName ){
+                    me.sMapLayer.list[i].active = true;
+                }else{
+                    me.sMapLayer.list[i].active = false;
+                }
+        }
     })
 
 
