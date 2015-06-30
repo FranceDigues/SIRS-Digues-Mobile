96ff4a45991d616e1ebffef52e525825e8d0599d
rdardie <roch.dardie@geomatys.com>
2015-06-30 15:35:44 +0200

    feat  (desordre) :  visible layer list + right view structure

---
+++ SirsMobile/www/js/module_app/controller/Ctrl_Menu_Tribord_closableDesordre.js
@@ -0,0 +1,25 @@
+/**
+ * Created by roch dardie on 29/06/15.
+ */
+
+
+
+
+angular.module('module_app.controllers.menus.tribord.closableDesordre', [])
+
+    .controller('cClosableDesordre', function cClosableDesordre ($scope, $log, sContext) {
+
+        var me = this;
+        me.sContext = sContext;
+
+
+        me.drawOnMap = function(){
+
+        }
+        me.runWithGps =function(){
+
+        }
+
+
+
+    })
---
+++ SirsMobile/www/js/module_app/controller/Ctrl_Menu_Tribord_editableLayer.js
@@ -0,0 +1,28 @@
+/**
+ * Created by roch dardie on 29/06/15.
+ */
+
+
+
+
+angular.module('module_app.controllers.menus.tribord.editableLayer', [])
+
+    .controller('cEditableLayer', function cEditableLayer ($scope, $log, sContext,sAppLayer) {
+
+        var me = this;
+        me.sContext = sContext;
+        me.sAppLayer = sAppLayer;
+
+        me.layerforAddDesordre = null;
+
+        me.drawOnMap = function(){
+
+        }
+        me.runWithGps =function(){
+
+        }
+
+
+
+
+    })
