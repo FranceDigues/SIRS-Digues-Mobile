6e5d33b49c523aedf660becd6ff67802446d2c4b
rdardie <roch.dardie@geomatys.com>
2015-07-13 17:12:57 +0200

    chore  (lib) :  update dependencies

---
+++ SirsMobile/nodeConf.sh
@@ -0,0 +1,12 @@
+sudo npm install -g bower
+sudo npm install -g gulp
+sudo npm install -g shelljs
+sudo npm install -g gulp-util
+sudo npm install -g gulp-sass
+sudo npm install -g gulp-rename
+sudo npm install -g gulp-minify-css
+sudo npm install -g gulp-concat
+
+sudo  npm install --save-dev main-bower-files
+
+sudo  npm install --save-dev event-stream
