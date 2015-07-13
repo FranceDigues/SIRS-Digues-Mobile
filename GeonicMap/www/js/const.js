f8a192c415280be8ec30f23ef667ce1672f30f48
rdardie <roch.dardie@geomatys.com>
2015-07-14 00:21:22 +0200

    chore (g):clean

--- GeonicMap/www/js/const.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Roch Dardie on 15/03/15.
- */
-
-//var pOktmp =[[[-120.313,33.118],[-109.766,20.281],[-87.617,32.823],[-101.504,51.385],[-121.895,51.385],[-129.453,46.784],[-120.313,33.118]  ]];
-var pOktmp = [[[-5e6, -1e6], [-4e6, 1e6], [-3e6, -1e6]]];
-//var tt = {1"source": { "type": "GeoJSON",        "geojson": {        "object": {            "type": "FeatureCollection",                "features": [            {                "type": "Feature",                "id": "cacheBox",                "properties": {                "name": "cacheBox"            },                "geometry": {                "type": "Polygon",                    "coordinates": [[[-120.313,33.118],[-109.766,20.281],[-87.617,32.823],[-101.504,51.385],[-121.895,51.385],[-129.453,46.784],[-120.313,33.118]  ]]}}]},"projection": "EPSG:3857"}},"style": {    "fill": {        "color": "rgba(255, 0, 0, 0.6)"    },    "stroke": {        "color": "white",            "width": 3    }},"visible": true,    "opacity": 0.6};
