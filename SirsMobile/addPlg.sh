7512a8cab3d1d3c9dacc5559af7bd3d850ad14d1
fbernard <fabien.bernard@geomatys.com>
2015-11-27 12:22:38 +0100

    chore(*): remove unused cordova plugins and fix some css issues

--- SirsMobile/addPlg.sh
+++ SirsMobile/addPlg.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 
-# Update source of external plugins.
-pushd ../../EXTERNAL_GIT_DEP
-    for i in ./* ; do
-        if [ -d "$i" ]; then
-            pushd $i
-                git pull
-            popd
-        fi
-    done
-popd
-
 # Install plugins from repository.
 ionic plugin add com.ionic.keyboard@1.0.4;
 ionic plugin add cordova-plugin-console@1.0.2;
 ionic plugin add cordova-plugin-crosswalk-webview@1.4.0;
 ionic plugin add cordova-plugin-device@1.1.0;
-ionic plugin add cordova-plugin-dialogs@1.2.0;
 ionic plugin add cordova-plugin-file@3.0.0;
 ionic plugin add cordova-plugin-fileopener@1.0.3;
-ionic plugin add cordova-plugin-file-transfer@1.4.0;
 ionic plugin add cordova-plugin-media@1.0.1;
 ionic plugin add cordova-plugin-media-capture@1.1.0;
 ionic plugin add cordova-plugin-network-information@1.1.0;
