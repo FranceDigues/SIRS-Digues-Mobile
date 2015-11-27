b98c6944b6d41f615050d49399f9677cfb02ed79
fbernard <fabien.bernard@geomatys.com>
2015-11-27 18:28:23 +0100

    refactor(*): remove roch cv from source... and other files...

--- SirsMobile/www/data/staticCfg.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Roch Dardie on 30/03/15.
- */
-var cMaskId = "7b3c967f37ef8f43cf0f1e33c2003cd2";
-var cLayerBase = "7b3c967f37ef8f43cf0f1e33c2005a7f";
-var cFormTest = "7b3c967f37ef8f43cf0f1e33c200831b";
