cc7d852d3575281e3f5fb742cf3352cd822910ad
fbernard <fabien.bernard@geomatys.com>
2016-02-08 12:08:24 +0100

    feat(geoloaction): zoom on position when the geolocation is enabled

--- SirsMobile/www/js/controller/Ctrl_menu.js
+++ SirsMobile/www/js/controller/Ctrl_menu.js
@@ -1,20 +1,31 @@
 angular.module('app.controllers.menu', [])
 
-    .controller('MenuController', function BabordMenuController(ContextService, GeolocationService, EditionService, SidePanelService) {
+    .controller('MenuController', function BabordMenuController(ContextService, GeolocationService, EditionService, SidePanelService, currentView) {
 
         var self = this;
 
         self.settings = ContextService.getValue().settings;
 
-        (self.updateGeolocation = function() {
+        (self.updateGeolocation = function(centerOn) {
             if (self.settings.geolocation) {
-                GeolocationService.start();
+                var promise = GeolocationService.start();
+                if (centerOn !== false) {
+                    promise.then(centerOnLocation);
+                }
             } else {
                 GeolocationService.stop();
             }
-        })();
+        })(false);
 
         self.sEdition = EditionService;
 
         self.setBabordView = SidePanelService.setBabordView;
+
+
+        function centerOnLocation(location) {
+            var coors = location.coords,
+                center = ol.proj.transform([coors.longitude, coors.latitude], 'EPSG:4326', 'EPSG:3857');
+            currentView.setCenter(center);
+            currentView.setZoom(16);
+        }
     });
