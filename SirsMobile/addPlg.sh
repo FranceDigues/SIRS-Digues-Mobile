062409e13511b750740501c4c2a93d73ef67d4ad
Hilmi Bouallegue <hilmi.bouallegue@geomatys.com>
2016-09-29 10:07:34 +0200

    Fix(App) refactor the app to work with the deprecated dependecies.

--- SirsMobile/addPlg.sh
+++ SirsMobile/addPlg.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Install plugins from repository.
-ionic plugin add com.ionic.keyboard@1.0.4;
+#ionic plugin add com.ionic.keyboard@1.0.4; // Deprecated
+#ionic plugin add org.apache.cordova.camera@0.3.6;// Deprecated
+
+ionic plugin add ionic-plugin-keyboard@1.0.4;
 ionic plugin add cordova-plugin-console@1.0.2;
 ionic plugin add cordova-plugin-crosswalk-webview@1.4.0;
 ionic plugin add cordova-plugin-device@1.1.0;
@@ -12,10 +15,15 @@ ionic plugin add cordova-plugin-media-capture@1.1.0;
 ionic plugin add cordova-plugin-network-information@1.1.0;
 ionic plugin add cordova-plugin-splashscreen@3.0.0;
 ionic plugin add cordova-plugin-whitelist@1.2.0;
-ionic plugin add org.apache.cordova.camera@0.3.6;
+ionic plugin add cordova-plugin-camera@0.3.6;
+
 
 # Install plugins from their sources.
 ionic plugin add ../pulgin/cacheMap/;
 ionic plugin add ../../EXTERNAL_GIT_DEP/Canvas2ImagePlugin;
+
+# @hb Modification
+ionic plugin add cordova-plugin-googleplayservices;
+ionic plugin add cordova-android-support-v4;
 ionic plugin add ../../EXTERNAL_GIT_DEP/geolocation-cordova;
 
