16038679b6e00a62c638b82550f50f18731bbb1f
hbouallegue <hilmi.bouallegue@geomatys.com>
2017-09-21 10:56:22 +0200

    feat(*): add the case of edit object to download photos from attachments

--- SirsMobile/www/js/controller/Ctrl_object_edit.js
+++ SirsMobile/www/js/controller/Ctrl_object_edit.js
@@ -296,6 +296,69 @@ angular.module('app.controllers.object_edit', [])
 
         self.saveNote = savePicture;
 
+        self.loaded = {};
+
+        b64toBlob = function (b64Data, contentType, sliceSize) {
+            contentType = contentType || '';
+            sliceSize = sliceSize || 512;
+
+            var byteCharacters = atob(b64Data);
+            var byteArrays = [];
+
+            for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+                var byteNumbers = new Array(slice.length);
+                for (var i = 0; i < slice.length; i++) {
+                    byteNumbers[i] = slice.charCodeAt(i);
+                }
+
+                var byteArray = new Uint8Array(byteNumbers);
+
+                byteArrays.push(byteArray);
+            }
+
+            var blob = new Blob(byteArrays, {type: contentType});
+            return blob;
+        };
+
+        self.loadImage = function (photo) {
+            var image_url = self.getPhotoPath(photo);
+            $.ajax({url:image_url,type:'HEAD',
+                error:function () {
+                    if(self.doc._attachments && self.doc._attachments[photo.id] && self.doc._attachments[photo.id].data){
+                        var fileName = photo.id+'.png';
+                        var blobImage = b64toBlob(self.doc._attachments[photo.id].data,'image/png');
+
+                        window.resolveLocalFileSystemURL(self.mediaPath, function(targetDir) {
+                            targetDir.getFile(fileName, {create:true}, function(file) {
+                                file.createWriter(function(fileWriter) {
+                                    fileWriter.write(blobImage);
+                                    window.setTimeout(function () {
+                                        self.loaded[photo.id] = true;
+                                        $scope.$digest();
+                                    },10);
+                                }, function(){
+                                    console.log('cannot write the data to the file');
+                                });
+                            });
+                        });
+                    } else {
+                        console.log("no attachment exit to load image");
+                    }
+                },
+                success:function () {
+                    window.setTimeout(function () {
+                        window.setTimeout(function () {
+                            self.loaded[photo.id] = true;
+                            $scope.$digest();
+                        },10);
+                    },10);
+                }
+            });
+
+        };
+
         self.getPhotoPath = function(photo) {
             var path = photo.chemin.replace(/\\/g, '/');
             if (path.charAt(0) !== '/') {
