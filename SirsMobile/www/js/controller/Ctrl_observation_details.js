c388c94fadb0734b7dad368c386876e234bb9a26
hbouallegue <hilmi.bouallegue@geomatys.com>
2017-09-21 10:22:14 +0200

    feat(*): in the case of photo not in the device & the photo in in the attachment of the observation the system add the photo to the device

--- SirsMobile/www/js/controller/Ctrl_observation_details.js
+++ SirsMobile/www/js/controller/Ctrl_observation_details.js
@@ -1,11 +1,10 @@
 
 angular.module('app.controllers.observation_details', [])
 
-    .controller('ObservationDetailsController', function ObservationDetailsController($ionicPlatform, sContext,
+    .controller('ObservationDetailsController', function ObservationDetailsController($ionicPlatform, sContext, $scope,
                                                                                       SidePanelService, LocalDocument, $rootScope) {
 
         var self = this;
-
 
         self.doc = sContext.selectedObservation;
 
@@ -15,12 +14,78 @@ angular.module('app.controllers.observation_details', [])
 
         self.objectId = sContext.selectedObject._id;
 
+        self.loaded = {};
+
+        b64toBlob = function (b64Data, contentType, sliceSize) {
+            contentType = contentType || '';
+            sliceSize = sliceSize || 512;
+
+            var byteCharacters = atob(b64Data);
+            var byteArrays = [];
+
+            for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+                var byteNumbers = new Array(slice.length);
+                for (var i = 0; i < slice.length; i++) {
+                    byteNumbers[i] = slice.charCodeAt(i);
+                }
+
+                var byteArray = new Uint8Array(byteNumbers);
+
+                byteArrays.push(byteArray);
+            }
+
+            var blob = new Blob(byteArrays, {type: contentType});
+            return blob;
+        };
+
+        self.loadImage = function (photo) {
+            var image_url = self.getPhotoPath(photo);
+            $.ajax({url:image_url,type:'HEAD',
+                    error:function () {
+                        if(self.doc._attachments && self.doc._attachments[photo.id] && self.doc._attachments[photo.id].data){
+                            var fileName = photo.id+'.png';
+                            var blobImage = b64toBlob(self.doc._attachments[photo.id].data,'image/png');
+
+                            window.resolveLocalFileSystemURL(self.mediaPath, function(targetDir) {
+                                targetDir.getFile(fileName, {create:true}, function(file) {
+                                    file.createWriter(function(fileWriter) {
+                                        fileWriter.write(blobImage);
+                                        window.setTimeout(function () {
+                                            self.loaded[photo.id] = true;
+                                            $scope.$digest();
+                                        },10);
+                                    }, function(){
+                                        console.log('cannot write the data to the file');
+                                    });
+                                });
+                            });
+                        } else {
+                            console.log("no attachment exit to load image");
+                        }
+                    },
+                    success:function () {
+                        window.setTimeout(function () {
+                            window.setTimeout(function () {
+                                self.loaded[photo.id] = true;
+                                $scope.$digest();
+                            },10);
+                        },10);
+                    }
+                });
+
+        };
+
         self.getPhotoPath = function(photo) {
             var path = photo.chemin.replace(/\\/g, '/');
             if (path.charAt(0) !== '/') {
                 path = '/' + path;
             }
-            return self.mediaPath + path;
+
+            var image_url = self.mediaPath + path;
+
+            return image_url;
         };
 
         self.backToDisorderDetails = function() {
