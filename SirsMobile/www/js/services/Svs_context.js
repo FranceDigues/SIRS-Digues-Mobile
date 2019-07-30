eae85a82c1caf1472cd0027b7f8a1fdb3a298fb8
hbouallegue <hilmi.bouallegue@geomatys.com>
2019-07-30 12:27:20 +0200

    fix(*): fix use abstract label

--- SirsMobile/www/js/services/Svs_context.js
+++ SirsMobile/www/js/services/Svs_context.js
@@ -43,7 +43,7 @@ angular.module('app.services.context', ['app.services.utils', 'app.services.dao'
 
         // Others.
         lastLocation: null,
-        version: '0.2.57'
+        version: '0.2.58'
     })
 
     .service('ContextService', function ContextService($rootScope, $location, LocalStorageItem, defaultContext) {
