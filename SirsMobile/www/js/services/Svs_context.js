4a663df263664ab5e994184e71f1e91bf10bca52
Hilmi BOUALLAGUE <hilmi.bouallegue@geomatys.com>
2020-09-29 11:31:00 +0200

    refactor(): update the app version

--- SirsMobile/www/js/services/Svs_context.js
+++ SirsMobile/www/js/services/Svs_context.js
@@ -43,7 +43,7 @@ angular.module('app.services.context', ['app.services.utils', 'app.services.dao'
 
         // Others.
         lastLocation: null,
-        version: '0.2.73'
+        version: '0.2.74'
     })
 
     .service('ContextService', function ContextService($rootScope, $location, LocalStorageItem, defaultContext) {
